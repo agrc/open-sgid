dd70845a7f813bfce36ee080f66f8e25ada3f1ee
steveoh <sgourley@utah.gov>
2019-11-07 18:38:56 -0700

    feat: add logger, verbosity cli option, dry-run and skip if exists
    tables are getting a shape field still...

--- src/cloudb/__main__.py
+++ src/cloudb/__main__.py
@@ -4,10 +4,10 @@
 cloudb
 
 Usage:
-  cloudb create db [--name=<name>]
-  cloudb create schema [--schemas=<name>]
-  cloudb create admin-user
-  cloudb import [--skip-schema=<names>...]
+  cloudb create db [--name=<name> --verbosity=<level>]
+  cloudb create schema [--schemas=<name> --verbosity=<level>]
+  cloudb create admin-user [--verbosity=<level>]
+  cloudb import [--skip-schema=<names>... --dry-run --verbosity=<level> --skip-if-exists]
 
 Arguments:
   name - all or any of the other iso categories
@@ -22,10 +22,14 @@ from osgeo import gdal, ogr
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 from . import config
+from .logger import Logger
+
+LOG = Logger()
+CONNECTION_TABLE_CACHE = {}
 
 
 def execute_sql(sql, connection):
-    print(f'  executing {sql}')
+    LOG.info(f'  executing {sql}')
 
     with psycopg2.connect(**connection) as conn:
         with conn.cursor() as cursor:
@@ -35,7 +39,7 @@ def execute_sql(sql, connection):
 
 
 def create_db(name, owner):
-    print(f'creating database {name}')
+    LOG.info(f'creating database {name}')
 
     sql = dedent(f'''
         CREATE DATABASE {name}
@@ -48,7 +52,7 @@ def create_db(name, owner):
     admin_connection = config.DBO_CONNECTION
     admin_connection['database'] = 'postgres'
 
-    print(f'  executing {sql}')
+    LOG.info(f'  executing {sql}')
 
     with psycopg2.connect(**admin_connection) as conn:
         conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
@@ -56,7 +60,7 @@ def create_db(name, owner):
         with conn.cursor() as cursor:
             cursor.execute(sql)
 
-    print('enabling postgis')
+    LOG.info('enabling postgis')
 
     execute_sql('CREATE EXTENSION postgis;', config.DBO_CONNECTION)
 
@@ -94,13 +98,14 @@ def create_schemas(schemas):
             sql.append(f'GRANT ALL ON SCHEMA {name} TO {config.ADMIN["name"]}')
             sql.append(f'GRANT USAGE ON SCHEMA {name} TO public')
 
-        print(f'creating schemas for {sql}')
+        LOG.info(f'creating schemas for {sql}')
         with conn.cursor() as cursor:
             cursor.execute(';'.join(sql))
 
         conn.commit()
 
-def _get_tables(connection, skip_schemas):
+
+def _get_tables(connection_string, skip_schemas):
     layer_schema_map = []
     exclude_schemas = ['sde', 'meta']
     exclude_fields = ['objectid', 'fid', 'globalid', 'gdb_geomattr_data']
@@ -108,15 +113,24 @@ def _get_tables(connection, skip_schemas):
     if skip_schemas and len(skip_schemas) > 0:
         exclude_schemas.extend(skip_schemas)
 
-    for qualified_layer in connection:
+    LOG.verbose('connecting to database')
+    connection = gdal.OpenEx(connection_string)
+
+    LOG.verbose('getting layer count')
+    table_count = connection.GetLayerCount()
+
+    LOG.info(f'found {Fore.YELLOW}{table_count}{Fore.RESET} total tables')
+
+    for table_index in range(table_count):
+        qualified_layer = connection.GetLayerByIndex(table_index)
         schema, layer = qualified_layer.GetName().split('.')
         schema = schema.lower()
         layer = layer.lower()
 
-        print(f'checking {Fore.CYAN}{schema}.{layer}{Fore.RESET}...')
+        LOG.debug(f'- {Fore.CYAN}{schema}.{layer}{Fore.RESET}')
 
         if schema in exclude_schemas:
-            print(f'{Fore.RED}- skipping:{Fore.RESET} {schema}')
+            LOG.verbose(f' {Fore.RED}- skipping:{Fore.RESET} {schema}')
 
             continue
 
@@ -129,41 +143,78 @@ def _get_tables(connection, skip_schemas):
             field_name = field.GetName().lower()
 
             if field_name in exclude_fields:
-                print(f'  {Fore.YELLOW}- skipping:{Fore.RESET} {field_name}')
-
-                field.Destroy()
+                LOG.verbose(f'  {Fore.YELLOW}- skipping:{Fore.RESET} {field_name}')
 
                 continue
 
             fields.append(field_name)
 
-            field.Destroy()
-
         layer_schema_map.append((schema, layer, fields))
 
-        del definition
         del qualified_layer
 
-    print(f'sorting map. found {Fore.GREEN}{len(layer_schema_map)}{Fore.RESET} layers')
+    LOG.info(f'found {Fore.GREEN}{len(layer_schema_map)}{Fore.RESET} tables for import')
     layer_schema_map.sort(key=lambda items: items[0])
+
+    connection = None
 
     return layer_schema_map
 
 
-def import_data(skip_schemas):
-    gdal.UseExceptions()
+def _check_if_exists(connection_string, schema, table):
+    LOG.debug('checking cache')
+
+    if connection_string in CONNECTION_TABLE_CACHE and len(CONNECTION_TABLE_CACHE[connection_string]) > 0:
+        LOG.verbose('cache populated')
+        return f'{schema}.{table}' in CONNECTION_TABLE_CACHE[connection_string]
+
+    LOG.verbose('connecting to database')
+    #: gdal.open gave a 0 table count
+    connection = ogr.Open(connection_string)
+
+    LOG.verbose('getting layer count')
+    table_count = connection.GetLayerCount()
+
+    LOG.debug(f'found {Fore.YELLOW}{table_count}{Fore.RESET} total tables for cache')
+    CONNECTION_TABLE_CACHE.setdefault(connection_string, [])
+
+    for table_index in range(table_count):
+        qualified_layer = connection.GetLayerByIndex(table_index)
+
+        if qualified_layer:
+            name = qualified_layer.GetName()
+
+            LOG.verbose(f'found layer: {name}')
+
+            CONNECTION_TABLE_CACHE[connection_string].append(name)
+
+    found = False
+    if f'{schema}.{table}' in CONNECTION_TABLE_CACHE[connection_string]:
+        found = True
+
+    del qualified_layer
+    connection = None
+
+    return found
+
+
+def import_data(skip_schemas, if_not_exists, dry_run):
+    gdal.SetConfigOption('MSSQLSPATIAL_LIST_ALL_TABLES', 'YES')
     gdal.SetConfigOption('PG_USE_COPY', 'YES')
+    gdal.SetConfigOption('PG_USE_POSTGIS', 'YES')
+    gdal.SetConfigOption('PG_LIST_ALL_TABLES', 'YES')
 
     cloud_db = config.format_ogr_connection(config.DBO_CONNECTION)
     internal_sgid = config.get_source_connection()
 
-    connection = ogr.Open(internal_sgid)
+    layer_schema_map = _get_tables(internal_sgid, skip_schemas)
 
-    layer_schema_map = _get_tables(connection, skip_schemas)
-
-    print(f'{Fore.BLUE}inserting layers...{Fore.RESET}')
+    LOG.info(f'{Fore.BLUE}inserting layers...{Fore.RESET}')
 
     for schema, layer, fields in layer_schema_map:
+        if if_not_exists and _check_if_exists(cloud_db, schema, layer):
+            LOG.info(f' -skipping {Fore.RED}{schema}.{layer}{Fore.RESET}: already exists')
+            continue
 
         sql = f'SELECT objectid FROM "{schema}.{layer}"'
 
@@ -203,17 +254,23 @@ def import_data(skip_schemas):
             ],
         )
 
-        print(f'inserting {Fore.MAGENTA}{layer}{Fore.RESET} into {Fore.BLUE}{schema}{Fore.RESET} with {Fore.CYAN}{sql}{Fore.RESET}')
+        LOG.info(f'inserting {Fore.MAGENTA}{layer}{Fore.RESET} into {Fore.BLUE}{schema}{Fore.RESET}')
+        LOG.debug(f'with {Fore.CYAN}{sql}{Fore.RESET}')
 
-        result = gdal.VectorTranslate(
-            cloud_db,
-            internal_sgid,
-            options=pg_options
-        )
+        if not dry_run:
+            result = gdal.VectorTranslate(
+                cloud_db,
+                internal_sgid,
+                options=pg_options
+            )
 
-        print(f'{Fore.GREEN}- done{Fore.RESET}')
+            import pdb; pdb.set_trace()
 
-        del result
+            del result
+
+        LOG.info(f'{Fore.GREEN}- done{Fore.RESET}')
+
+    LOG.info(f'{Fore.GREEN} Completed!')
 
 
 def create_public_user(props):
@@ -245,7 +302,9 @@ def main():
     '''
     init()
     args = docopt(__doc__, version='1.0.0')
-    print(f'{Back.WHITE}{Fore.BLACK}{args}{Back.RESET}{Fore.RESET}')
+
+    LOG.init(args['--verbosity'])
+    LOG.debug(f'{Back.WHITE}{Fore.BLACK}{args}{Back.RESET}{Fore.RESET}')
 
     if args['create']:
         if args['schema']:
@@ -268,7 +327,7 @@ def main():
             return create_db(name.lower(), config.DBO)
 
     if args['import']:
-        return import_data(args['--skip-schema'])
+        return import_data(args['--skip-schema'], args['--skip-if-exists'], args['--dry-run'])
 
     return 1
 
---
+++ src/cloudb/logger.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# * coding: utf8 *
+'''
+logger.py
+A module that lets you filter log statements
+'''
+
+
+class Logger():
+    '''a very stupid logger
+    '''
+    severity = ['VERBOSE', 'DEBUG', 'INFO', 'WARNING', 'FATAL']
+    verbosity = 'INFO'
+
+    def init(self, verbosity='INFO'):
+        if not verbosity:
+            return
+
+        self.verbosity = verbosity
+
+    def verbose(self, value):
+        self._print(value, 'VERBOSE')
+
+    def debug(self, value):
+        self._print(value, 'DEBUG')
+
+    def info(self, value):
+        self._print(value, 'INFO')
+
+    def warning(self, value):
+        self._print(value, 'WARNING')
+
+    def fatal(self, value):
+        self._print(value, 'FATAL')
+
+    def _print(self, value, verbosity):
+        if self.severity.index(verbosity) >= self.severity.index(self.verbosity):
+            print(value)
