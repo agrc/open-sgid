478c6a0bcf84fed793a97275560074ad1c066ecf
steveoh <sgourley@utah.gov>
2021-12-09 11:20:14 -0700

    feat: add indexes for parcel searches

--- src/cloudb/index.py
+++ src/cloudb/index.py
@@ -7,6 +7,11 @@ index - A module that creates sql indexes
 DEFAULT = 'create index if not exists idx_{2}_{0} on {1}.{2} ({0});'
 FUZZY = 'create index if not exists trgm_idx_{2}_{0} on {1}.{2} using gin ({0} gin_trgm_ops);'
 
+PARCEL_LAYERS = [
+    'beaver', 'box_elder', 'cache', 'carbon', 'daggett', 'davis', 'duchesne', 'emery', 'garfield', 'grand', 'iron',
+    'juab', 'kane', 'millard', 'morgan', 'piute', 'rich', 'salt_lake', 'san_juan', 'sanpete', 'sevier', 'summit',
+    'tooele', 'uintah', 'utah', 'wasatch', 'washington', 'wayne', 'weber'
+]
 INDEXES = {
     'location.address_points': [
         DEFAULT.format('fulladd', 'location', 'address_points'),
@@ -29,3 +34,6 @@ INDEXES = {
         DEFAULT.format('owner', 'cadastre', 'land_ownership'),
     ],
 }
+
+for county in PARCEL_LAYERS:
+    INDEXES[f'cadastre.{county}'] = [DEFAULT.format('parcel_id', 'cadastre', county)]
